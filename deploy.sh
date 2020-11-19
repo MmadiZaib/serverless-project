8926640da8b10ea2c62e6d568cd75dadfb08592b
Zaïb <zaib.mmadi@meero.com>
2020-11-19 14:10:59 +0100

    add ci/cd

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# [TESTING]
+#env="dev"
+
+# Deploy serverless backend
+serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v
+
+# Gather service endpoint from cloud formation output
+serviceEndpoint=$( aws cloudformation describe-stacks --stack-name auction-service-$env --query "Stacks[0].Outputs[?OutputKey=='ServiceEndpoint'].{ServiceEndpoint:OutputValue}" --output text )
+
+# Generate env config file for our frontend
+echo " 
+// Environment configuration for $env
+const API_ENDPOINT = \"$serviceEndpoint\";
+"
