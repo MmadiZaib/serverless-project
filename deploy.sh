302d07b6c30e3eb59e11aa9dc8b3dd8bb25cb6b7
Zaïb <zaib.mmadi@meero.com>
2020-11-19 16:30:19 +0100

    update ci/cd

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 #env="dev"
 
 # Deploy serverless backend
-serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v
+serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-west-1
 
 # Gather service endpoint from cloud formation output
 serviceEndpoint=$( aws cloudformation describe-stacks --stack-name auction-service-$env --query "Stacks[0].Outputs[?OutputKey=='ServiceEndpoint'].{ServiceEndpoint:OutputValue}" --output text )
